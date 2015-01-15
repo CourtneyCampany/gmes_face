c94afe2dc73b21916cb07965c39c0b28bedc7bf8
Courtney Campany <courtneycampany@gmail.com>
2015-01-15 16:49:14 +1100

    added 2 scripts, one for calculated gm and the other that holds the functions

--- functions/tdl_licor_functions.R
+++ functions/tdl_licor_functions.R
@@ -7,7 +7,7 @@ tdlformat_func <- function(x){
   x$Date <- paste(x$calendar, x$clock, sep=" ")
   x$datetime <- strptime(x$Date,  format = "%Y-%m-%d  %H:%M:%S")
   
-  dfr <- x[,c("SiteOutput", "CorrConcA_Avg","CorrConcB_Avg",  "Corrdel13C_Avg", "datetime")]
+  dfr <- x[,c("SiteOutput", "CorrConcA_Avg","CorrConcB_Avg",  "Corrdel13C_Avg", "datetime", "licor")]
   
   return(dfr)
 }
@@ -22,7 +22,7 @@ licorformat_func <- function(x){
   x$datetime<- strptime(x$Date2, format = "%d/%m/%Y  %H:%M:%S")
   
   dfr <- x[,c(1:62,66)]
-  dfr$id <- paste(dfr$Tree, dfr$Canopy, dfr$CO2, sep="-")
+  dfr$id <- paste(dfr$Ring,dfr$Tree, dfr$Canopy, dfr$CO2, sep="-")
   dfr$id <- as.factor(dfr$id)
   dfr$O2 <- 21
   #sort the dfr to put o2 in the right place
@@ -36,7 +36,7 @@ licorformat_func <- function(x){
 timerange_func <- function(x, dfr){
   
   x_sp <- split(x, x$id)
-  dfr<- lapply(x_sp, function(x, dfr){
+  dfr<- lapply(x_sp, function(x){
       max.dt <- max(x$datetime)
       min.dt <- min(x$datetime)
       id<- unique(x$id)
@@ -51,23 +51,72 @@ timerange_func <- function(x, dfr){
 ###xsi calulcation for csv with tdl data (needs input of sample id still)
 xsicalc_func <- function(x){
   xsi_dfr <- x[x$SiteOutput != c(3,4),]
+  #give unique to ref and sample lines using odd/even tdl line #s
   xsi_dfr$line_id <- ifelse(xsi_dfr$SiteOutput %% 2 == 1,"a", "b")
   xsi_dfr$CO2_total <- (xsi_dfr$CorrConcA_Avg+xsi_dfr$CorrConcB_Avg)/(1-0.00474)
   #seperate ref and sample lines for calculations
   xsi_a <- xsi_dfr[xsi_dfr$line_id=="a",]
-  names(xsi_a)[4] <- "del13_ref" 
-  names(xsi_a)[7] <- "CO2_total_ref"
+  colnames(xsi_a)[(names(xsi_a) == "Corrdel13C_Avg")] <- "del13_ref"
+  colnames(xsi_a)[(names(xsi_a) == "CO2_total")] <- "CO2_total_ref"
+
   xsi_b <- xsi_dfr[xsi_dfr$line_id=="b",]
-  names(xsi_b)[4] <- "del13_samp"
-  names(xsi_b)[7] <- "CO2_total_samp"
+  colnames(xsi_b)[(names(xsi_b) == "Corrdel13C_Avg")] <- "del13_samp"
+  colnames(xsi_b)[(names(xsi_b) == "CO2_total")] <- "CO2_total_samp"
+  
   #new dfr with xsi, deltadiff, DELTA, and timestamp for matching
   deltadiff<- xsi_b$del13_samp - xsi_a$del13_ref
   xsi <- xsi_b$CO2_total_samp/(xsi_a$CO2_total_ref - xsi_b$CO2_total_samp)
-  
   xsi_calc <-data.frame(cbind(deltadiff, xsi))
   xsi_calc$DELTA <- (1000 * xsi_calc$xsi * xsi_calc$deltadiff)/(1000+xsi_b$del13_samp-(xsi_calc$xsi*xsi_calc$deltadiff))
+  
   #times back to this dataframe..which ones?
   xsi_calc$timeavg <- xsi_a$datetime-((xsi_a$datetime - xsi_b$datetime)/2)
+  xsi_calc$licor <- xsi_dfr$licor[1]
   
   return(xsi_calc)
 }
+
+###time match function to find minimum difference between two sets of datetimes-------------------------------------
+timematch <- function(time1, time2)abs(time1-time2)
+
+
+####gmes_func---------------------------------------------------------------------------------------------------------
+gmes_func <- function(xsi_dfr, licor_dfr, times_dfr, logtime=10,whichlicor="f2" ){
+  
+  ###subset licor_dfr by licor used
+  licor_dfr2 <- licor_dfr[licor_dfr$licor == whichlicor,]
+  licor_dfr2 <- droplevels(licor_dfr2)
+  
+  ###use times_dfr to add "sample id" to xsi dfr based datetime interval
+  xsi_dfr$id <- "no_sample"
+  
+  for(i in 1:nrow(times_dfr)){
+    
+    j <- which(xsi_dfr$timeavg >= times_dfr$min[i] & xsi_dfr$timeavg <= times_dfr$max[i])
+    xsi_dfr$id[j] <- as.character(times_dfr$id[i])
+  }
+  
+  xsi_samples <- xsi_dfr[xsi_dfr$id != "no_sample",]
+  
+  ###add closest matched licor time "datetime_match" to xsi_dfr with timematch func (min diff)
+  ii<- sapply(1:nrow(xsi_samples), function(i)which.min(timematch(xsi_samples$timeavg[i], licor_dfr2$datetime)))
+  xsi_samples$datetime_match <- licor_dfr2$datetime[ii]
+  
+  ###subsets of licor_dfr2 based on time match + 2 logs +- (nrow=5 in each list)
+  licor_ids <- lapply(1:nrow(xsi_samples),
+                      function(k) {
+                        id_split <- licor_dfr2[licor_dfr2$datetime <= (xsi_samples$datetime_match[k]+(2.4*logtime)) &
+                                                 licor_dfr2$datetime >= (xsi_samples$datetime_match[k]-(2.4*logtime)), ]
+                      })
+  
+  ###mean of each list, convert to dfr, cbind with xsi&DELTA from xsi_dfr (nrow must be equal)
+  licor_ids_datefix <- llply(licor_ids, function(x) c(x$datetime <- as.numeric(x$datetime), return(x)))
+  licor_ids_agg <- llply(licor_ids_datefix,function(x) summaryBy(.~id+Date+Empty_id, data=x, FUN=mean, keep.names=TRUE))
+  licor_agg_dtfix <- llply(licor_ids_agg, function(x) c(x$datetime <- as.POSIXct(x$datetime,
+                                                                                 origin= '1970-01-01', tz=""), return(x)))
+  
+  licor_agg<- rbind.fill(licor_agg_dtfix)
+  ###nrow needs to be equal (STOP)
+  licor_xsi <- cbind(licor_agg, xsi_samples[,c("xsi", "DELTA")])
+}
+
--- gm_timematch.R
+++ gm_timematch.R
@@ -1,64 +1,36 @@
 source("functions/tdl_licor_functions.R")
 
-#### load packages and data---------------------------------------------------------------------------------------------
-library(data.table)
-
-licor_master <- read.csv("licor_gm_master.csv")
-tdl_oct22_2121 <- read.csv("tdl_oct22_2122.csv")
-
-
-####format tdl data (will be csv covering a day and a ref/sample line with samples within)------------------------------
-tdl_oct22_2121 <- tdlformat_func(tdl_oct22_2121)
-####calculatre xsi/Delta with times
-xsi_oct22_2121 <- xsicalc_func(tdl_oct22_2121)
-
+####read data---------------------------------------------------------------------------------------------
+licor_master <- read.csv("licor_face_master.csv")
+tdl_oct22_2122 <- read.csv("tdl_oct22_2122.csv")
+tdl_oct22_1920 <- read.csv("tdl_oct22_1920.csv")
 
 ####format master licor file, will have two dataframes one with all data, and one with times for matching---------------
 licor_gmes <- licorformat_func(licor_master)
+
+###now run time range function if get samples id and time range
 licor_times <- timerange_func(licor_gmes)
 
 
-####once files are formatted subset the tdl files to range of licor datetime for any sample id-------------------------
+####format tdl data (will be csv covering a day and a ref/sample line with samples within)------------------------------
+tdl_oct22_2122 <- tdlformat_func(tdl_oct22_2122)
+tdl_oct22_1920 <- tdlformat_func(tdl_oct22_1920)
 
-#make a sample specific tdl and licor dfr
-upper_116_tdl <- xsi_oct22_2121[xsi_oct22_2121$timeavg > as.POSIXct('2014-10-22 11:35:22') & 
-                           xsi_oct22_2121$timeavg < as.POSIXct('2014-10-22 11:50:11 '), ]
-  upper_116_tdl$id <- "116_upper_amb"
-
-upper_116_licor <- licor_gmes[licor_gmes$id == "116-upper-amb",]
-  
-####time match upper116
-
-timematch <- function(datetime1, datetime2) 
-
-# One variable
-df <- data.frame(latitude=seq(30.25, 50.25, by=0.5))
-df$MAT <- runif(nrow(df),20,25)
-
-plotloc <- data.frame(latitude = runif(10, 31,49))
-
-diffun <- function(lat1, lat2)abs(lat1 - lat2)
-ii <- sapply(1:nrow(plotloc), function(i)which.min(diffun(plotloc$latitude[i], df$latitude)))
-plotloc$MAT <- df$MAT[ii]
-
-# Two variables
-df2 <- expand.grid(latitude=seq(40.25, 50.25, by=0.5), longitude=c(110, 115, 120))
-df2$MAT <- 1:nrow(df2)
-
-diffun2 <- function(lat1, lat2, lon1, lon2)abs(lat1-lat2) + abs(lon1-lon2)
-
-plotloc2 <- data.frame(latitude = runif(10, 41,49), longitude=runif(10, 110,120))
-
-ii <- sapply(1:nrow(plotloc2), 
-             function(i)which.min(diffun2(plotloc2$latitude[i], 
-                                          df2$lat, plotloc2$longitude[i], df2$lon)))
-plotloc2$MAT <- df2$MAT[ii]
-
-  
-  
-  
+####calculatre xsi/Delta with times by licor id
+xsi_oct22_f2 <- xsicalc_func(tdl_oct22_2122)
+xsi_oct22_f4 <- xsicalc_func(tdl_oct22_1920)
 
 
-licor_gm2 <- subset(licor_gm, datetime >= min.dt & datetime <= max.dt)
+###packages and functiosn for gmes_func
+library(data.table)
+library(plyr)
+
+
+gm_oct22_f2 <- gmes_func(xsi_oct22_f2, licor_gmes, licor_times, whichlicor="f2")
+gm_oct22_f4 <- gmes_func(xsi_oct22_f4, licor_gmes, licor_times, whichlicor="f4")
+
+
+
+
 
 
