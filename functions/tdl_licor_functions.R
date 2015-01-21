e5b958bfc17c959fd57fdc3409e65c100245151c
Courtney Campany <courtneycampany@gmail.com>
2015-01-21 12:43:33 +1100

    added general formatting for licor function which creates a proper dataframe for excel gm calc
    generalization of row matching and datetimes not working with nrow method

--- functions/tdl_licor_functions.R
+++ functions/tdl_licor_functions.R
@@ -1,18 +1,43 @@
+chooseidfunc <- function(dfr, ){
+  
+  
+  
+}
+
 
 ####licor formating function------------------------------------------------------------------------------------------
 licorformat_func <- function(x){
-  x$calendar <- strptime(x$Date, format = "%d/%m/%Y")
+  
+  licorfirst<- which(colnames(x) == "Obs")
+  licorlast<- which(colnames(x) == "AHs.Cs")
+  
+  x$calendar <- strptime(x$Date, format = "%d/%m/%Y", tz="UTC")
   x$calendar <- as.character(x$Date)
   x$clock <- as.character(x$HHMMSS)
   x$Date2 <- paste(x$calendar, x$clock, sep=" ")
-  x$datetime<- strptime(x$Date2, format = "%d/%m/%Y  %H:%M:%S")
+  x$datetime<- strptime(x$Date2, format = "%d/%m/%Y  %H:%M:%S",  tz="UTC")
+  dtcol <- which(colnames(x) == "datetime")
+  licorcol <- which(colnames(x) == "licor")
   
-  dfr <- x[,c(1:62,66)]
-  dfr$id <- paste(dfr$Ring,dfr$Tree, dfr$Canopy, dfr$CO2, sep="-")
-  dfr$id <- as.factor(dfr$id)
+  x$id <-paste(x$Ring,x$Tree, x$Canopy, x$CO2, sep="-")
+  x$id <- as.factor(x$id)
+  idcol <- which(colnames(x) == "id")
+  
+  
+  #subset dataset to keep licor values, id, and datetime
+  dfr <- x[,c(licorfirst:licorlast, licorcol, idcol, dtcol)]
   dfr$O2 <- 21
   #sort the dfr to put o2 in the right place
-  dfr <- dfr[,c(1:9, 65, 10:64)]
+  idcol2 <- which(colnames(dfr) == "id") 
+  dtcol2 <- which(colnames(dfr) == "datetime")
+  ocol <- which(colnames(dfr) == "O2")
+  licorcol2 <- which(colnames(dfr) == "licor")
+  licorfirst2<- which(colnames(dfr) == "Obs")
+  licorlast2<- which(colnames(dfr) == "AHs.Cs")
+  beforeO<- which(colnames(dfr) == "FTime")
+  afterO<- which(colnames(dfr) == "EBal.")
+  
+  dfr <- dfr[, c(licorfirst2:beforeO,ocol, afterO:licorlast2, licorcol2, idcol2, dtcol2 )]
   return(dfr)
 }
 
@@ -20,11 +45,11 @@ licorformat_func <- function(x){
 #tdl formatting function-----------------------------------------------------------------------------------------------
 
 tdlformat_func <- function(x){
-  x$calendar <- strptime(x$TIMESTAMP, format = "%d/%m/%Y")
+  x$calendar <- strptime(x$TIMESTAMP, format = "%d/%m/%Y",  tz="UTC")
   x$calendar <- as.character(x$calendar)
   x$clock <- as.character(x$TIME)
   x$Date <- paste(x$calendar, x$clock, sep=" ")
-  x$datetime <- strptime(x$Date,  format = "%Y-%m-%d  %H:%M:%S")
+  x$datetime <- strptime(x$Date,  format = "%Y-%m-%d  %H:%M:%S",  tz="UTC")
   
   dfr <- x[,c("SiteOutput", "CorrConcA_Avg","CorrConcB_Avg",  "Corrdel13C_Avg", "datetime", "licor")]
   
@@ -49,6 +74,7 @@ xsicalc_func <- function(x){
   #new dfr with xsi, deltadiff, DELTA, and timestamp for matching
   deltadiff<- xsi_b$del13_samp - xsi_a$del13_ref
   xsi <- xsi_b$CO2_total_samp/(xsi_a$CO2_total_ref - xsi_b$CO2_total_samp)
+  
   xsi_calc <-data.frame(cbind(deltadiff, xsi))
   xsi_calc$DELTA <- (1000 * xsi_calc$xsi * xsi_calc$deltadiff)/(1000+xsi_b$del13_samp-(xsi_calc$xsi*xsi_calc$deltadiff))
   
@@ -82,7 +108,9 @@ timematch <- function(time1, time2)abs(time1-time2)
 
 
 ####gmes_func---------------------------------------------------------------------------------------------------------
-gmes_func <- function(xsi_dfr, licor_dfr, times_dfr, logtime=10,whichlicor="f2" ){
+#for the moment i removed licor times
+
+gmes_func <- function(xsi_dfr, licor_dfr, times_dfr, licorrows=5,whichlicor="f2" ){
   
   ###subset licor_dfr by licor used
   licor_dfr2 <- licor_dfr[licor_dfr$licor == whichlicor,]
@@ -98,23 +126,34 @@ gmes_func <- function(xsi_dfr, licor_dfr, times_dfr, logtime=10,whichlicor="f2" 
   }
   
   xsi_samples <- xsi_dfr[xsi_dfr$id != "no_sample",]
+  if(any(xsi_samples$xsi >20)) warning ("You have XSI values over the acceptable limit")
   
   ###add closest matched licor time "datetime_match" to xsi_dfr with timematch func (min diff)
   ii<- sapply(1:nrow(xsi_samples), function(i)which.min(timematch(xsi_samples$timeavg[i], licor_dfr2$datetime)))
   xsi_samples$datetime_match <- licor_dfr2$datetime[ii]
   
   ###subsets of licor_dfr2 based on time match + 2 logs +- (nrow=5 in each list)
+#   licor_ids <- lapply(1:nrow(xsi_samples),
+#                       function(k) {
+#                         id_split <- licor_dfr2[licor_dfr2$datetime <= (xsi_samples$datetime_match[k]+(2.4*logtime)) &
+#                                                licor_dfr2$datetime >= (xsi_samples$datetime_match[k]-(2.4*logtime)), ]
+#                       })
+  thisrow <- ii 
+  delr <- (licorrows-1)/2
+  licordfr[(thisrow-delr):(thisrow+delr),]
+  
   licor_ids <- lapply(1:nrow(xsi_samples),
                       function(k) {
-                        id_split <- licor_dfr2[licor_dfr2$datetime <= (xsi_samples$datetime_match[k]+(2.4*logtime)) &
-                                                 licor_dfr2$datetime >= (xsi_samples$datetime_match[k]-(2.4*logtime)), ]
+                        id_split <- licor_dfr2[(thisrow-delr:(thisrow+delr),]
                       })
+  
+  
   
   ###mean of each list, convert to dfr, cbind with xsi&DELTA from xsi_dfr (nrow must be equal)
   licor_ids_datefix <- llply(licor_ids, function(x) c(x$datetime <- as.numeric(x$datetime), return(x)))
   licor_ids_agg <- llply(licor_ids_datefix,function(x) summaryBy(.~id+Date+Empty_id, data=x, FUN=mean, keep.names=TRUE))
   licor_agg_dtfix <- llply(licor_ids_agg, function(x) c(x$datetime <- as.POSIXct(x$datetime,
-                                                                                 origin= '1970-01-01', tz=""), return(x)))
+                                                                                 origin= '1970-01-01', tz="UTC"), return(x)))
   
   licor_agg<- rbind.fill(licor_agg_dtfix)
   ###nrow needs to be equal (STOP)
--- gm_timematch.R
+++ gm_timematch.R
@@ -1,5 +1,10 @@
 source("functions/tdl_licor_functions.R")
 
+###required packakges
+library(data.table)
+library(plyr)
+library(doBy)
+
 ####read data---------------------------------------------------------------------------------------------
 licor_master <- read.csv("licor_face_master.csv")
 tdl_oct22_2122 <- read.csv("tdl_oct22_2122.csv")
@@ -21,14 +26,105 @@ xsi_oct22_f2 <- xsicalc_func(tdl_oct22_2122)
 xsi_oct22_f4 <- xsicalc_func(tdl_oct22_1920)
 
 
-###packages and functiosn for gmes_func
-library(data.table)
-library(plyr)
-
-
+##gm ready dataframe function
 gm_oct22_f2 <- gmes_func(xsi_oct22_f2, licor_gmes, licor_times, whichlicor="f2")
 gm_oct22_f4 <- gmes_func(xsi_oct22_f4, licor_gmes, licor_times, whichlicor="f4")
 
+#####Testing------------------------------
+
+licor_dfr2 <- licor_gmes[licor_gmes$licor == "f2",]
+licor_dfr2 <- droplevels(licor_dfr2)
+
+xsi_oct22_f2$id <- "no_sample"
+
+for(i in 1:nrow(licor_times)){
+  
+  j <- which(xsi_oct22_f2$timeavg >= licor_times$min[i] & xsi_oct22_f2$timeavg <= licor_times$max[i])
+  xsi_oct22_f2$id[j] <- as.character(licor_times$id[i])
+}
+
+xsi_samples <- xsi_oct22_f2[xsi_oct22_f2$id != "no_sample",]
+
+
+ii<- sapply(1:nrow(xsi_samples), function(i)which.min(timematch(xsi_samples$timeavg[i], licor_dfr2$datetime)))
+xsi_samples$datetime_match <- licor_dfr2$datetime[ii]
+
+licorrows <-5
+
+thisrow <- ii
+
+
+delr <- (licorrows-1)/2
+
+licor_ids <- lapply(1:length(thisrow),
+                    function(k) {
+                      id_split <- licor_dfr2[(thisrow[k]-delr):(thisrow[k]+delr),]
+                    })
+
+###mean of each list, convert to dfr, cbind with xsi&DELTA from xsi_dfr (nrow must be equal)
+licor_ids_datefix <- llply(licor_ids, function(x) c(x$datetime <- as.numeric(x$datetime), return(x)))
+licor_ids_agg <- llply(licor_ids_datefix,function(x) summaryBy(.~id+Date+Empty_id, data=x, FUN=mean, keep.names=TRUE))
+licor_agg_dtfix <- llply(licor_ids_agg, function(x) c(x$datetime <- as.POSIXct(x$datetime,
+                                                  origin= '1970-01-01', tz="UTC"), return(x)))
+
+licor_agg<- rbind.fill(licor_agg_dtfix)
+
+###nrow needs to be equal (STOP)
+licor_xsi <- cbind(licor_agg, xsi_samples[,c("xsi", "DELTA")])
+
+
+####gmes_func---------------------------------------------------------------------------------------------------------
+#for the moment i removed licor times
+
+gmes_func <- function(xsi_dfr, licor_dfr, times_dfr, licorrows=5,whichlicor="f2" ){
+  
+  ###subset licor_dfr by licor used
+  licor_dfr2 <- licor_dfr[licor_dfr$licor == whichlicor,]
+  licor_dfr2 <- droplevels(licor_dfr2)
+  
+  ###use times_dfr to add "sample id" to xsi dfr based datetime interval
+  xsi_dfr$id <- "no_sample"
+  
+  for(i in 1:nrow(times_dfr)){
+    
+    j <- which(xsi_dfr$timeavg >= times_dfr$min[i] & xsi_dfr$timeavg <= times_dfr$max[i])
+    xsi_dfr$id[j] <- as.character(times_dfr$id[i])
+  }
+  
+  xsi_samples <- xsi_dfr[xsi_dfr$id != "no_sample",]
+  if(any(xsi_samples$xsi >20)) warning ("You have XSI values over the acceptable limit")
+  
+  ###add closest matched licor time "datetime_match" to xsi_dfr with timematch func (min diff)
+  ii<- sapply(1:nrow(xsi_samples), function(i)which.min(timematch(xsi_samples$timeavg[i], licor_dfr2$datetime)))
+  xsi_samples$datetime_match <- licor_dfr2$datetime[ii]
+  
+  ###subsets of licor_dfr2 based on time match + 2 logs +- (nrow=5 in each list)
+  #   licor_ids <- lapply(1:nrow(xsi_samples),
+  #                       function(k) {
+  #                         id_split <- licor_dfr2[licor_dfr2$datetime <= (xsi_samples$datetime_match[k]+(2.4*logtime)) &
+  #                                                licor_dfr2$datetime >= (xsi_samples$datetime_match[k]-(2.4*logtime)), ]
+  #                       })
+  thisrow <- ii 
+  delr <- (licorrows-1)/2
+  licordfr[(thisrow-delr):(thisrow+delr),]
+  
+  licor_ids <- lapply(1:nrow(xsi_samples),
+                      function(k) {
+                        id_split <- licor_dfr2[(thisrow[k]-delr:(thisrow[k]+delr),]
+                      })
+  
+  
+  
+  ###mean of each list, convert to dfr, cbind with xsi&DELTA from xsi_dfr (nrow must be equal)
+  licor_ids_datefix <- llply(licor_ids, function(x) c(x$datetime <- as.numeric(x$datetime), return(x)))
+  licor_ids_agg <- llply(licor_ids_datefix,function(x) summaryBy(.~id+Date+Empty_id, data=x, FUN=mean, keep.names=TRUE))
+  licor_agg_dtfix <- llply(licor_ids_agg, function(x) c(x$datetime <- as.POSIXct(x$datetime,
+                                                                                 origin= '1970-01-01', tz="UTC"), return(x)))
+  
+  licor_agg<- rbind.fill(licor_agg_dtfix)
+  ###nrow needs to be equal (STOP)
+  licor_xsi <- cbind(licor_agg, xsi_samples[,c("xsi", "DELTA")])
+}
 
 
 
