c7f6ea40b340e4fd151a38e94869dbaa2adcc28c
Courtney Campany <courtneycampany@gmail.com>
2017-05-24 14:47:21 +1000

    made new file

--- gmes_format.R
+++ gmes_format.R
@@ -1,28 +1,60 @@
 
+
+# read and format data ----------------------------------------------------
+
 gmes <-read.csv("master_data_file_clean.csv")
   gmes$Date <- as.Date(gmes$Date)
 
-  
-amb <- gmes[gmes$co2meas=="amb",]
+#anatomy is doubled up so split dfr by CO2 measurement level (instantaneous effect) 
+gmes_amb <- gmes[gmes$co2meas == "amb",]
+gmes_elev <- gmes[gmes$co2meas == "elev",]
 
+#or split by growth CO2 (FACE treatments)
+gmes_amb2 <- gmes[gmes$co2grow == "amb",]
+gmes_elev2 <- gmes[gmes$co2grow == "elev",]
+
+
+# basic plots -------------------------------------------------------------
 palette(c("black", "red"))
 pchs <- c(16,17)
+gmlab <-expression(italic(g)[m]~~(mol~m^-2~s^-1))
+condlab <- expression(italic(g)[s]~~(mol~m^-2~s^-1))
+photolab <- expression(italic(A)[n]~~(mu*mol~m^-2~s^-1))
+leglab <- c(expression(aCO[2]), expression(eCO[2]), "lower canopy", "upper canopy")
+allcols <- c("black", "red", "black", "black")
+legpch <- c(16,16,16,17)
 
-plot(gmes~mesolay.mean, data=amb, col=co2grow, pch=pchs[amb$canopy])
+#try a bunch, keep the ones that may should a trend to analyze
 
-plot(gmes~leafw, data=amb, col=co2grow, pch=pchs[amb$canopy])
+#Physiology (growth CO2)
+par(mar=c(5,5,1,1), cex=1.25, las=1, cex.axis=.8, cex.lab=1, mgp=c(3,1,0))
 
-plot(gmes~mesolay.mean, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(Photo ~ gmes, data=gmes, col=co2grow, pch=pchs[canopy], ylim=c(0,30), xlim=c(0,.5), xlab=gmlab, ylab=photolab)
+  legend("topleft", leglab, pch=legpch, col=allcols,inset = 0.01, bty='n',cex=.8)
 
-plot(gmes~epi_up, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(Photo ~ mean_gs, data=gmes, col=co2grow, pch=pchs[canopy], ylim=c(0,30), xlim=c(0,.4), xlab=condlab, ylab=photolab)
+  legend("topleft", leglab, pch=legpch, col=allcols,inset = 0.01, bty='n',cex=.8)
 
-plot(gmes~epi_low, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(gmes ~ mean_gs, data=gmes, col=co2grow, pch=pchs[canopy], ylim=c(0,.5), xlim=c(0,.4),xlab=condlab, ylab=gmlab)
+  legend("topleft", leglab, pch=legpch, col=allcols,inset = 0.01, bty='n',cex=.8)
 
-plot(gmes~meso.mean, data=amb, col=co2grow, pch=pchs[amb$canopy])
+#Anatomy (growth CO2)
+  
+plot(gmes~mesolay.mean, data=gmes_amb2, col=co2grow, pch=pchs[canopy])
 
-plot(gmes~length.par1.mean, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(gmes~leafw, data=gmes_amb2, col=co2grow, pch=pchs[canopy])
 
-plot(gmes~mean_gs, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(gmes~mesolay.mean, data=gmes_amb2, col=co2grow, pch=pchs[canopy])
 
-plot(gmes~mean_cc, data=amb, col=co2grow, pch=pchs[amb$canopy])
+plot(gmes~epi_up, data=gmes_amb2, col=co2grow, pch=pchs[canopy])
+
+plot(gmes~epi_low, data=gmes_amb2, col=co2grow, pch=pchs[amb$canopy])
+
+plot(gmes~meso.mean, data=gmes_amb2, col=co2grow, pch=pchs[amb$canopy])
+
+plot(gmes~length.par1.mean, data=gmes_amb2, col=co2grow, pch=pchs[amb$canopy])
+
+plot(gmes~mean_gs, data=gmes_amb2, col=co2grow, pch=pchs[amb$canopy])
+
+plot(gmes~mean_cc, data=gmes_amb2, col=co2grow, pch=pchs[amb$canopy])
 
