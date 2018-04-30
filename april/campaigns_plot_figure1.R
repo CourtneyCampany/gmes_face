b3e47975460b37534b2c7d232a4320bdf2630d39
Courtney Campany <courtneycampany@gmail.com>
2018-04-30 11:43:14 -0400

    new campaign figure

--- april/campaigns_plot_figure1.R
+++ april/campaigns_plot_figure1.R
@@ -25,210 +25,114 @@ jlab <- expression(atop(Apparent~J[max],~~(mu*mol~m^-2~s^-1)))
 vlab <- expression(atop(Apparent~V[cmax],~~(mu*mol~m^-2~s^-1)))
 daterange <- c(as.Date("2014-04-01"), as.Date("2016-04-01"))
 datelab <- as.Date("2016-04-01")
+pchs2 <- c(1,1,16,16)
 
-
-#plotting with color------
+#plotting---------
 windows()
 par(oma=c(5,7,3,1), mfrow=c(4,2), mar=c(0,0,0,0), pch=16, lwd=1.5)
 
 #photosynthesis-aco2
-plot(photo ~ Date, data=aco2_low, col=canopycols[1], type="o",
-     xlab="",ylab="", ylim=c(0, 25), xaxt='n', cex=1.5, xlim=daterange)
-points(photo ~ Date, data=aco2_upp, col=canopycols[2], cex=1.5, type="o")
+plot(photo ~ Date, data=aco2_low, type="b", pch=pchs2,lty=3,
+     xlab="",ylab="", ylim=c(0, 25), xaxt='n', cex=1.25, xlim=daterange)
+points(photo ~ Date, data=aco2_upp,  cex=1.25, type="b",pch=pchs2)
 axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, photo, Date, photo+se.photo, angle=90, 
-                    length=.03, col=canopycols[Position]))
-with(aco2, arrows(Date, photo, Date, photo-se.photo, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(aco2, arrows(Date, photo, Date, photo+se.photo, angle=90, lwd=1,
+                    length=.03))
+with(aco2, arrows(Date, photo, Date, photo-se.photo, angle=90, lwd=1,
+                  length=.03))
 mtext(photolab, side=2, line=4, cex=.8)
-legend("bottomleft", legend=c("Upper Canopy", "Lower Canopy"), pch=16,bty='n',
-       col=c(canopycols[2], canopycols[1]), inset=.01)
+legend("bottomleft", legend=c("Mature Leaves", "New Leaves", "Upper Canopy", "Lower Canopy"),
+       bty='n', col="black", inset=.01, pch=c(1,16,NA,NA), lty=c(NA, NA, 1,3))
 mtext(expression(Ambient~CO[2]), side=3, line=1, cex=.8)
 text(datelab, 24, "A", cex=1.2)
 
 #photosynthesis-eco2
-plot(photo ~ Date, data=eco2_low, col=canopycols[1], type="o",
-     xlab="",ylab="", ylim=c(0, 25), xaxt='n', yaxt='n', cex=1.5, xlim=daterange)
-points(photo ~ Date, data=eco2_upp, col=canopycols[2],cex=1.5, type="o")
+plot(photo ~ Date, data=eco2_low,  type="b", pch=pchs2,lty=3,
+     xlab="",ylab="", ylim=c(0, 25), xaxt='n', yaxt='n', cex=1.25, xlim=daterange)
+points(photo ~ Date, data=eco2_upp, cex=1.25, type="b",pch=pchs2)
 axis(2, labels=FALSE, tcl=.5)
-with(eco2, arrows(Date, photo, Date, photo+se.photo, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(eco2, arrows(Date, photo, Date, photo-se.photo, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(eco2, arrows(Date, photo, Date, photo+se.photo, angle=90, lwd=1,
+                  length=.03))
+with(eco2, arrows(Date, photo, Date, photo-se.photo, angle=90, lwd=1,
+                  length=.03))
 mtext(expression(Elevated~CO[2]), side=3, line=1, cex=.8)
 text(datelab, 24, "B", cex=1.2)
 
 #vcmax-aco2
-plot(Vcmax ~ Date, data=aco2_low, col=canopycols[1],xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.5, type='o')
-points(Vcmax ~ Date, data=aco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(Vcmax ~ Date, data=aco2_low, xlim=daterange,
+     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.25, type="b", pch=pchs2,lty=3)
+points(Vcmax ~ Date, data=aco2_upp,  type="b",pch=pchs2, cex=1.25)
 axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(aco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(aco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, lwd=1,
+                  length=.03))
+with(aco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, lwd=1,
+                  length=.03))
 mtext(vlab, side=2, line=3,cex=.8)
 text(datelab, 150, "C", cex=1.2)
 
 #vcmax-eco2
-plot(Vcmax ~ Date, data=eco2_low, col=canopycols[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(Vcmax ~ Date, data=eco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(Vcmax ~ Date, data=eco2_low, xlim=daterange,
+     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.25,yaxt='n', type="b", pch=pchs2,lty=3)
+points(Vcmax ~ Date, data=eco2_upp, type="b",pch=pchs2, cex=1.25)
 axis(2, labels=FALSE, tcl=.5)
-with(eco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(eco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(eco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, lwd=1,
+                  length=.03))
+with(eco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, lwd=1,
+                  length=.03))
 text(datelab, 150, "D", cex=1.2)
 
 #jmax-aco2
-plot(Jmax ~ Date, data=aco2_low, col=canopycols[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.5, type='o')
-points(Jmax ~ Date, data=aco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(Jmax ~ Date, data=aco2_low,  xlim=daterange,
+     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.25, type="b", pch=pchs2,lty=3)
+points(Jmax ~ Date, data=aco2_upp, type="b",pch=pchs2, cex=1.25)
 axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(aco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(aco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, lwd=1,
+                  length=.03))
+with(aco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, lwd=1,
+                  length=.03 ))
 mtext(jlab, side=2, line=3,cex=.8)
 text(datelab, 200, "E", cex=1.2)
 
 #jmax-eco2
-plot(Jmax ~ Date, data=eco2_low, col=canopycols[1],xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(Jmax ~ Date, data=eco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(Jmax ~ Date, data=eco2_low, xlim=daterange,
+     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.25,yaxt='n', type="b", pch=pchs2,lty=3)
+points(Jmax ~ Date, data=eco2_upp, type="b",pch=pchs2, cex=1.25)
 axis(2, labels=FALSE, tcl=.5)
-with(eco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(eco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(eco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, lwd=1,
+                  length=.03))
+with(eco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, lwd=1,
+                  length=.03))
 text(datelab, 200, "F", cex=1.2)
 
 #lma-aco2
-plot(LMA ~ Date, data=aco2_low, col=canopycols[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 225), xaxt='n', cex=1.5, type='o')
-points(LMA ~ Date, data=aco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(LMA ~ Date, data=aco2_low,  xlim=daterange,
+     xlab="",ylab="", ylim=c(0, 225), xaxt='n', cex=1.25,type="b", pch=pchs2,lty=3)
+points(LMA ~ Date, data=aco2_upp,  type="b",pch=pchs2, cex=1.25)
 axis(1, labels=FALSE, at=unique(aco2$Date))
 text(unique(aco2$Date), par("usr")[3] - 45, srt = 45,
      labels = campainlab, xpd = NA, cex=1.2)
-with(aco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(aco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(aco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, lwd=1,
+                  length=.03))
+with(aco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, lwd=1,
+                  length=.03))
 mtext(lmalab, side=2, line=4, cex=.8)
 text(datelab, 210, "G",cex=1.2)
 
 #lma-eco2
-plot(LMA ~ Date, data=eco2_low, col=canopycols[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 225), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(LMA ~ Date, data=eco2_upp, col=canopycols[2], type='o', cex=1.5)
+plot(LMA ~ Date, data=eco2_low, xlim=daterange, xlab="",ylab="", ylim=c(0, 225), 
+     xaxt='n', cex=1.25,yaxt='n', type="b", pch=pchs2,lty=3)
+points(LMA ~ Date, data=eco2_upp, type="b",pch=pchs2, cex=1.25)
 axis(1, labels=FALSE, at=unique(aco2$Date))
 text(unique(aco2$Date), par("usr")[3] - 45, srt = 45,
      labels = campainlab, xpd = NA, cex=1.2)
 axis(2, labels=FALSE, tcl=.5)
-with(eco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, 
-                  length=.03, col=canopycols[Position]))
-with(eco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, 
-                  length=.03, col=canopycols[Position]))
+with(eco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, lwd=1,
+                  length=.03))
+with(eco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, lwd=1,
+                  length=.03))
 text(datelab, 210, "H",cex=1.2)
 
 dev.copy2pdf(file= "output/campaigns.pdf")
 dev.off()
 
 
-#plotting with no color------
-windows()
-par(oma=c(5,7,1,1), mfrow=c(4,2), mar=c(0,0,0,0), lwd=1)
-
-#photosynthesis-aco2
-plot(photo ~ Date, data=aco2_low, pch=pchs[1], type="o",
-     xlab="",ylab="", ylim=c(0, 25), xaxt='n', cex=1.5, xlim=daterange)
-points(photo ~ Date, data=aco2_upp, pch=pchs[2], cex=1.5, type="o")
-axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, photo, Date, photo+se.photo, angle=90, 
-                  length=.03))
-with(aco2, arrows(Date, photo, Date, photo-se.photo, angle=90, 
-                  length=.03))
-mtext(photolab, side=2, line=4, cex=.8)
-legend("bottomleft", legend=c("Upper Canopy", "Lower Canopy"),
-       pch=c(pchs[2],pchs[1]),bty='n', inset=.01, pt.cex=1.5)
-
-#photosynthesis-eco2
-plot(photo ~ Date, data=eco2_low, pch=pchs[1], type="o",
-     xlab="",ylab="", ylim=c(0, 25), xaxt='n', yaxt='n', cex=1.5, xlim=daterange)
-points(photo ~ Date, data=eco2_upp, pch=pchs[2],cex=1.5, type="o")
-axis(2, labels=FALSE)
-with(eco2, arrows(Date, photo, Date, photo+se.photo, angle=90, 
-                  length=.03))
-with(eco2, arrows(Date, photo, Date, photo-se.photo, angle=90, 
-                  length=.03))
-#vcmax-aco2
-plot(Vcmax ~ Date, data=aco2_low, pch=pchs[1],xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.5, type='o')
-points(Vcmax ~ Date, data=aco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, 
-                  length=.03))
-with(aco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, 
-                  length=.03))
-mtext(vlab, side=2, line=3,cex=.8)
-
-#vcmax-eco2
-plot(Vcmax ~ Date, data=eco2_low, pch=pchs[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 160), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(Vcmax ~ Date, data=eco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(2, labels=FALSE)
-with(eco2, arrows(Date, Vcmax, Date, Vcmax+se.Vcmax, angle=90, 
-                  length=.03))
-with(eco2, arrows(Date, Vcmax, Date, Vcmax-se.Vcmax, angle=90, 
-                  length=.03))
-
-#jmax-aco2
-plot(Jmax ~ Date, data=aco2_low, pch=pchs[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.5, type='o')
-points(Jmax ~ Date, data=aco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(1, labels=FALSE, at=unique(aco2$Date))
-with(aco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, 
-                  length=.03))
-with(aco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, 
-                  length=.03))
-mtext(jlab, side=2, line=3,cex=.8)
-
-#jmax-eco2
-plot(Jmax ~ Date, data=eco2_low, pch=pchs[1],xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 210), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(Jmax ~ Date, data=eco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(2, labels=FALSE)
-with(eco2, arrows(Date, Jmax, Date, Jmax+se.Jmax, angle=90, 
-                  length=.03))
-with(eco2, arrows(Date, Jmax, Date, Jmax-se.Jmax, angle=90, 
-                  length=.03))
-
-#lma-aco2
-plot(LMA ~ Date, data=aco2_low, pch=pchs[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 225), xaxt='n', cex=1.5, type='o')
-points(LMA ~ Date, data=aco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(1, labels=FALSE, at=unique(aco2$Date))
-text(unique(aco2$Date), par("usr")[3] - 40, srt = 45,
-     labels = campainlab, xpd = NA, cex=1.2)
-with(aco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, 
-                  length=.03))
-with(aco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, 
-                  length=.03))
-mtext(lmalab, side=2, line=4, cex=.8)
-
-#lma-eco2
-plot(LMA ~ Date, data=eco2_low, pch=pchs[1], xlim=daterange,
-     xlab="",ylab="", ylim=c(0, 225), xaxt='n', cex=1.5,yaxt='n', type='o')
-points(LMA ~ Date, data=eco2_upp, pch=pchs[2], type='o', cex=1.5)
-axis(1, labels=FALSE, at=unique(aco2$Date))
-text(unique(aco2$Date), par("usr")[3] - 40, srt = 45,
-     labels = campainlab, xpd = NA, cex=1.2)
-axis(2, labels=FALSE)
-with(eco2, arrows(Date, LMA, Date, LMA+se.LMA, angle=90, 
-                  length=.03))
-with(eco2, arrows(Date, LMA, Date, LMA-se.LMA, angle=90, 
-                  length=.03))
-
-dev.copy2pdf(file= "output/campaigns2.pdf")
-dev.off()
